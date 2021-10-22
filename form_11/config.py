3bd64b7745d8bfc7e530a154c7adf1fb8509361a
Michael Futch <michaelfutch@berniesanders.com>
2021-10-22 10:18:24 -0400

    clean up gitignore

--- form_11/config.py
+++
@@ -1,9 +0,0 @@
-config_vars = {
-    'project_id': 'cbac-306316',
-    'output_dataset_id': 'cbac_wordpress',
-    'output_table_name': 'observation_endpoint',
-    'sql_file_path': 'observation_endpoint.sql',
-    'CLIENT_KEY': 'ck_435d6de33aec58cd5c7af138cb69df6fe918a9e2',
-    'CLIENT_SECRET': 'cs_194eab268c0ab952dedea960f8698e3402fccc5d',
-    'credentials': 'cbac-306316-6aa67853d0f5.json'
-}
